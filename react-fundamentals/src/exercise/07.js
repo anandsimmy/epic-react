96fdabf7ec6107f5e5e3735e9b6acc1b67d87982
Anand Simmy <anandsimmy7@gmail.com>
2021-06-08 10:04:49 +0530

    epic react course work

--- react-fundamentals/src/exercise/07.js
+++ react-fundamentals/src/exercise/07.js
@@ -28,9 +28,9 @@ function App() {
         add item
       </button>
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
-        {items.map(item => (
+        {items.map((item, index) => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={index}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
