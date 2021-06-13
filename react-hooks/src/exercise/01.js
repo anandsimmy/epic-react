d4378231e06fbf311c56d11f9b00234305795d30
Anand Simmy <anandsimmy7@gmail.com>
2021-06-13 21:38:33 +0530

    epic react course work

--- react-hooks/src/exercise/01.js
+++ react-hooks/src/exercise/01.js
@@ -5,17 +5,19 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName]= React.useState('')
 
-  function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+  const nameRef= React.useRef()
+
+  function handleChange() {
+    setName(nameRef.current.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input ref={nameRef} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
