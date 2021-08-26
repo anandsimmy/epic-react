ba8f0140a9392a0563670badaccd07f3618ac593
Anand Simmy <anandsimmy7@gmail.com>
2021-08-26 08:17:57 +0530

    added context consumer hook

--- advanced-react-hooks/src/exercise/03.js
+++ advanced-react-hooks/src/exercise/03.js
@@ -5,12 +5,20 @@ import * as React from 'react'
 
 const CountContext= React.createContext();
 
+const useCount = () => {
+    const context = React.useContext(CountContext)
+    if(!context){
+      throw new Error('useCount must be used within a CountProvider')
+    }
+    return context
+};
+
 const CountProvider= ({ children }) => {
 
   const [count, setCount]= React.useState(0);
 
   return(
-    <CountContext.Provider value={{ count, setCount }}>
+    <CountContext.Provider value={[ count, setCount ]}>
       {children}
     </CountContext.Provider>
   )
@@ -18,13 +26,13 @@ const CountProvider= ({ children }) => {
 
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const { count } = React.useContext(CountContext)
+  const [ count ] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const { setCount } = React.useContext(CountContext)
+  const [, setCount ] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
