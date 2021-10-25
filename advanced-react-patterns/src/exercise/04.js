6e3e2236c4ba56e8e6d252347b666b704693bcc5
Anand Simmy <anandsimmy7@gmail.com>
2021-10-25 09:42:20 +0530

    epic react course work

--- advanced-react-patterns/src/exercise/04.js
+++ advanced-react-patterns/src/exercise/04.js
@@ -8,10 +8,8 @@ function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  const togglerProps = {'aria-pressed': on, onClick: toggle}
+  return {on, togglerProps}
 }
 
 function App() {
