160576bc566333892e7ef24b366349ec171211ce
Anand Simmy <anandsimmy7@gmail.com>
2022-03-07 07:52:34 +0530

    epic react course changes

--- advanced-react-patterns/src/exercise/06.js
+++ advanced-react-patterns/src/exercise/06.js
@@ -116,7 +116,7 @@ function App() {
     if (action.type === actionTypes.toggle && timesClicked > 4) {
       return
     }
-    setBothOn(null)
+    setBothOn(state.on)
     setTimesClicked(c => c + 1)
   }
 
